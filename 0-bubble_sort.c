f94b8c9ca375655a6d93da8456ede72fd3258957
Method Mkoma <gershommethod02@gmail.com>
2024-01-21 08:20:36 +0300

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,6 +10,10 @@ void bubble_sort(int *array, size_t size)
 	int t;
 	size_t i, j;
 
+	if (array == NULL || size == 0)
+	{
+		return;
+	}
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
