f2cf47d7d2132aea116122e1b79c60439a18877a
Method Mkoma <gershommethod02@gmail.com>
2024-01-22 20:18:04 +0300

    Task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,18 +6,38 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	const listint_t *tmp;
-	const int t;
+	listint_t *current, *prev, *next;
 
-	while (tmp != NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+
+	while (current != NULL)
 	{
-		if (tmp->n > tmp->next->n)
+		next = current->next;
+
+		while (current->prev != NULL && current->n < current->prev->n)
 		{
-			t = tmp->n;
-			tmp->n = tmp->next->n;
-			tmp->next->n = t;
+			prev = current->prev;
+			prev->next = current->next;
+
+			if (current->next != NULL)
+				current->next->prev = prev;
+
+			current->prev = prev->prev;
+			current->next = prev;
+
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			else
+				*list = current;
+
+			prev->prev = current;
+
 			print_list(*list);
 		}
+
+		current = next;
 	}
 }
-
--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
 void recursive_quick_sort(int *array, int low, int high, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif /*_SORT_H_*/
